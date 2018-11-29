3d197844ea63904e0a8ad97ca7d46e7ed2c269a9
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-11-29 18:06:30 +0100

    Little fixes and libraries

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,16 +1,20 @@
 package sample;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import stock.data.connection.IDataConnection;
+import stock.guice.StockModule;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
