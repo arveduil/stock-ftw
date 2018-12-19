a5515f4d73d6c9db395686dd0ba0ba0ac47c1378
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-12-19 16:28:51 +0100

    Additional Test added

--- src/test/java/StrategyApplicatorTest.java
+++ src/test/java/StrategyApplicatorTest.java
@@ -29,4 +29,32 @@ public class StrategyApplicatorTest {
         BigDecimal expected = budget.add(budget.multiply(new BigDecimal(0.2).multiply(end)));
         Assert.assertEquals(strategyResult.getResult(), expected);
     }
+
+    @Test
+    public void doubleStrategyApplicationTest() {
+        List<ExchangeRate> exchangeRates = Arrays.asList(new ExchangeRate(new BigDecimal(20.0), new Date()),
+                                                         new ExchangeRate(new BigDecimal(25.0), new Date()),
+                                                         new ExchangeRate(new BigDecimal(50.0), new Date()),
+                                                         new ExchangeRate(new BigDecimal(25.0), new Date()));
+        List<Strategy> strategies = Arrays.asList(
+                new Strategy(1, new BigDecimal(0.5), DecisionType.SELL, new BigDecimal(0.2)),
+                new Strategy(1, new BigDecimal(0.2), DecisionType.BUY, new BigDecimal(0.1)));
+
+        BigDecimal budget = new BigDecimal(1000000.0);
+        StrategyApplicator applicator = new StrategyApplicator(exchangeRates, strategies, budget);
+        applicator.applyStrategies();
+        Map<Strategy, StrategyResult> results = applicator.getStrategyResults();
+
+        StrategyResult strategyResult = results.get(strategies.get(0));
+        BigDecimal end = new BigDecimal(50.0);
+        BigDecimal expected = budget.add(budget.multiply(new BigDecimal(0.2).multiply(end)));
+
+        budget = expected;
+        StrategyResult strategyResult2 = results.get(strategies.get(1));
+        BigDecimal end2 = new BigDecimal(25.0);
+        BigDecimal expected2 = budget.subtract(budget.multiply(new BigDecimal(0.1).multiply(end)));
+
+        Assert.assertEquals(strategyResult.getResult(), expected);
+        Assert.assertEquals(strategyResult2.getResult(), expected2);
+    }
 }
