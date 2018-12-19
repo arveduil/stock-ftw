8e60896c544b00415bd0ee6542fa48c911ff1f78
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-12-19 16:41:51 +0100

    Strategy validation

--- src/main/java/ftw/strategy/model/Strategy.java
+++ src/main/java/ftw/strategy/model/Strategy.java
@@ -1,6 +1,8 @@
 package ftw.strategy.model;
 
 import ftw.strategy.DecisionType;
+import ftw.strategy.model.exception.InvalidStrategyValuesException;
+import ftw.strategy.model.validator.StrategyValidator;
 
 import java.math.BigDecimal;
 
@@ -14,11 +16,12 @@ public class Strategy {
 
     private BigDecimal investmentPercentage;
 
-    public Strategy(Integer checkInterval, BigDecimal change, DecisionType decisionType, BigDecimal investmentPercentage) {
+    public Strategy(Integer checkInterval, BigDecimal change, DecisionType decisionType, BigDecimal investmentPercentage) throws InvalidStrategyValuesException {
         this.checkInterval = checkInterval;
         this.change = change;
         this.decisionType = decisionType;
         this.investmentPercentage = investmentPercentage;
+        StrategyValidator.validate(this);
     }
 
     public Integer getCheckInterval() {
